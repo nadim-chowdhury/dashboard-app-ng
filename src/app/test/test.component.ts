c8582f7e36e3e920c6eec06fb942bd945bbb9417
Nadim <Nadim@chowdhury.com>
2023-08-12 00:14:57 +0600

    some code tested

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -3,8 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-test',
   templateUrl: './test.component.html',
-  styleUrls: ['./test.component.scss']
+  styleUrls: ['./test.component.scss'],
 })
 export class TestComponent {
+  //  title: "Hello World"
 
+  constructor() {
+    setTimeout(() => {
+      // this.title = 'Hello Nadim';
+    }, 2000);
+  }
 }
