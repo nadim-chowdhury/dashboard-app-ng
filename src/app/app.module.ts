cce0877c6bc124ea081bb186bc089f6d7d89a84b
Nadim <Nadim@chowdhury.com>
2023-08-12 12:21:07 +0600

    component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TestComponent } from './test/test.component';
+import { TodoComponent } from './todo/todo.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { DashboardHomeComponent } from './dashboard-home/dashboard-home.component';
+import { SmallCardComponent } from './small-card/small-card.component';
+import { BigCardComponent } from './big-card/big-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TestComponent
+    TestComponent,
+    TodoComponent,
+    NavbarComponent,
+    SidebarComponent,
+    DashboardHomeComponent,
+    SmallCardComponent,
+    BigCardComponent
   ],
   imports: [
     BrowserModule,
