6994679a1ca93cb5d13b5f293030d0db20f5470f
Nadim Chowdhury <nadim-chowdhury@outlook.com>
2023-08-14 23:17:52 +0600

    app routing implemented

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,48 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminPanelComponent } from './admin-panel/admin-panel.component';
+import { ForbiddenComponent } from './forbidden/forbidden.component';
+import { AuthGuard } from './auth/auth.guard';
+import { UserComponent } from './user/user.component';
+import { RegistrationComponent } from './user/registration/registration.component';
+import { LoginComponent } from './user/login/login.component';
+import { HomeComponent } from './home/home.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { EntryFormComponent } from './entry-form/entry-form.component';
 
-const routes: Routes = [];
+import { UserListComponent } from './user/user-list/user-list.component';
+import { ChangePasswordComponent } from './change-password/change-password.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/user/login', pathMatch: 'full' },
+  {
+    path: 'user',
+    component: UserComponent,
+    children: [
+      { path: 'login', component: LoginComponent },
+      { path: 'registration', component: RegistrationComponent },
+    ],
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+    canActivate: [AuthGuard],
+    children: [
+      { path: 'dashboard', component: DashboardComponent },
+      { path: 'changePassword', component: ChangePasswordComponent },
+      { path: 'appusers', component: UserListComponent },
+
+      { path: 'entryform', component: EntryFormComponent },
+    ],
+  },
+  { path: 'forbidden', component: ForbiddenComponent },
+  {
+    path: 'adminpanel',
+    component: AdminPanelComponent,
+    canActivate: [AuthGuard],
+    data: { permittedRoles: ['Admin'] },
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
